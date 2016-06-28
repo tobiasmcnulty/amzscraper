5cf630ff3866151891f3e82d93d9f2693cab2149
Tobias McNulty <tobias@caktusgroup.com>
2016-06-27 23:32:47 -0400

    add support for emailing PDF receipts and the Selenium ChromeDriver (replaces mechanize, which does not seem to be able to login to Amazon anymore as of May 2016)

--- amz.py
+++ amz.py
@@ -1,48 +1,47 @@
 import os
 import re
+import sys
 import time
 import random
 import hashlib
+import smtplib
 import argparse
 import memcache
 import datetime
 import itertools
-import mechanize
 import subprocess
 
 from bs4 import BeautifulSoup
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 
-class AmzScraper(object):
+class AmzMechanize(object):
+    """ Use Python mechanize to login and retrieve URLs. Broken as of May 2016. """
     headers = [
-        ('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.13) '
-                       'Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13')
+        # ('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.13) '
+        #                'Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13')
+        ('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 '
+                       '(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36')
     ]
-    base_url = 'https://www.amazon.com'
-    start_url = base_url+'/gp/css/history/orders/view.html?orderFilter=year-{yr}&startAtIndex=1000'
-    order_url = base_url+'/gp/css/summary/print.html/ref=od_aui_print_invoice?ie=UTF8&orderID={oid}'
 
-    order_date_re = re.compile(r'Order Placed:')
-    order_id_re = re.compile(r'orderID=([0-9-]+)')
-
-    def __init__(self, email, password, year, orders_dir, cache_timeout):
-        self.email = email
-        self.password = password
-        self.year = year
-        self.orders_dir = orders_dir
-        self.cache_timeout = cache_timeout
-        self.mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+    def __init__(self):
+        import mechanize
         self.br = mechanize.Browser()
         self.br.set_handle_robots(False)
         self.br.addheaders = self.headers
-        self.login()
 
-    def login(self):
+    def login(self, email, password):
         self.br.open('https://www.amazon.com')
-        self.br.follow_link(text_regex='Sign in')
+        resp = self.br.follow_link(text_regex='Sign in')
         self.br.select_form(nr=0)
-        self.br['email'] = self.email
-        self.br['password'] = self.password
+        self.br['email'] = email
+        self.br['password'] = password
         resp = self.br.submit()
         if resp.code != 200:
             raise Exception('Got invalid response code %s' % resp.code)
@@ -50,25 +49,105 @@ class AmzScraper(object):
             html = resp.get_data()
             soup = BeautifulSoup(html, 'lxml')
             err = soup.find_all('div', attrs={'id': 'message_error'})
-            msg = (err and err[0].renderContents() or html).strip()
-            raise Exception('Login failed for %s, %s: %s' % (self.email, self.password, msg))
+            warn = soup.find_all('div', attrs={'class': 'a-alert-content'})
+            msg = (err and err[0].renderContents()
+                   or (warn and warn[0].renderContents())
+                   or html).strip()
+            raise Exception('Login failed for %s, %s:\n%s' % (email, password, msg))
+
+    def get_url(self, url):
+        return self.br.open(url).get_data()
+
+    def clean_up(self):
+        pass
+
+
+class AmzChromeDriver(object):
+    """ Replacement driver to login to Amazon and download URLs using the Selenium ChromeDriver. """
+    def __init__(self):
+        from selenium import webdriver
+        self.driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+        self.driver.implicitly_wait(30)
+
+    def login(self, email, password):
+        driver = self.driver
+        driver.get("https://www.amazon.com/")
+        driver.find_element_by_css_selector("#nav-link-yourAccount > span.nav-line-1").click()
+        driver.find_element_by_id("ap_email").clear()
+        driver.find_element_by_id("ap_email").send_keys(email)
+        driver.find_element_by_id("ap_password").clear()
+        driver.find_element_by_id("ap_password").send_keys(password)
+        driver.find_element_by_id("signInSubmit").click()
+
+    def get_url(self, url):
+        self.driver.get(url)
+        return self.driver.page_source
+
+    def clean_up(self):
+        self.driver.quit()
+
+
+class Emailer(object):
+
+    def __init__(self, smtp_host, smtp_port, smtp_user, smtp_password):
+        self.smtp_host = smtp_host
+        self.smtp_port = int(smtp_port)
+        self.smtp_user = smtp_user
+        self.smtp_password = smtp_password
+
+    def send_mail(self, send_from, send_to, subject, text, files=None):
+        assert isinstance(send_to, list)
+        msg = MIMEMultipart()
+        msg['From'] = send_from
+        msg['To'] = COMMASPACE.join(send_to)
+        msg['Date'] = formatdate(localtime=True)
+        msg['Subject'] = subject
+        msg.attach(MIMEText(text))
+
+        for f in files or []:
+            with open(f, "rb") as fil:
+                print 'attaching', os.path.basename(f)
+                msg.attach(MIMEApplication(
+                    fil.read(),
+                    'pdf',
+                    name=os.path.basename(f),
+                    Content_Disposition='attachment; filename="%s"' % os.path.basename(f)
+                ))
+
+        smtp = smtplib.SMTP(self.smtp_host, self.smtp_port)
+        smtp.starttls()
+        smtp.login(self.smtp_user, self.smtp_password)
+        smtp.sendmail(send_from, send_to, msg.as_string())
+        smtp.close()
+
+
+class AmzScraper(object):
+
+    base_url = 'https://www.amazon.com'
+    start_url = base_url+'/gp/css/history/orders/view.html?orderFilter=year-{yr}&startAtIndex=1000'
+    order_url = base_url+'/gp/css/summary/print.html/ref=od_aui_print_invoice?ie=UTF8&orderID={oid}'
+
+    order_date_re = re.compile(r'Order Placed:')
+    order_id_re = re.compile(r'orderID=([0-9-]+)')
+
+    def __init__(self, year, user, password, dest_dir, cache_timeout, from_email,
+                 to_email, brcls=AmzChromeDriver, emailer=None):
+        self.year = year
+        self.orders_dir = dest_dir
+        self.cache_timeout = cache_timeout
+        self.from_email = from_email
+        self.to_email = to_email
+        self.emailer = emailer
+        self.mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+        self.br = brcls()
+        self.br.login(user, password)
 
     def _fetch_url(self, url, use_cache=True):
         key = hashlib.md5(url).hexdigest()
         val = use_cache and self.mc.get(key) or None
         if not val:
             print 'fetching %s from server (with random sleep)' % url
-            for x in range(3):
-                resp = self.br.open(url)
-                if resp.geturl() == url:
-                    break
-                print 'got unexpected URL (%s); expecting %s. attempting re-login...'\
-                      '' % (resp.geturl(), url)
-                self.login()
-            else:
-                raise Exception('Got an unexpected URL (most recently, %s) 3 times. Expected URL: '
-                                '%s' % (resp.geturl(), url))
-            val = resp.get_data()
+            val = self.br.get_url(url)
             # wait a little while so we don't spam Amazon
             time.sleep(random.randint(1, 5))
             self.mc.set(key, val, self.cache_timeout)
@@ -116,16 +195,30 @@ class AmzScraper(object):
             fn = 'amazon_order_{date}_{oid}.'.format(date=date, oid=oid) + '{ext}'
             fn = os.path.join(self.orders_dir, fn)
             with open(fn.format(ext='html'), 'w') as f:
-                f.write(html)
+                f.write(html.encode('ascii', 'ignore'))
             subprocess.check_call(['wkhtmltopdf', '--no-images', '--disable-javascript',
                                    fn.format(ext='html'), fn.format(ext='pdf')])
             os.remove(fn.format(ext='html'))
+            if self.from_email and self.to_email and self.emailer:
+                # email the PDF
+                subject = os.path.basename(fn.format(ext='pdf'))
+                body = ''
+                self.emailer.send_mail(self.from_email, [self.to_email], subject, body,
+                                       [os.path.abspath(fn.format(ext='pdf'))])
+            else:
+                print 'skipping email send for order %s' % oid
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Scrape an Amazon account and create order PDFs.')
     parser.add_argument('-u', '--user', required=True, help='Amazon.com username (email).')
     parser.add_argument('-p', '--password', required=True, help='Amazon.com password.')
+    parser.add_argument('--smtp-user', required=False, help='SMTP username (optional).')
+    parser.add_argument('--smtp-password', required=False, help='SMTP password (optional)')
+    parser.add_argument('--smtp-host', required=False, help='SMTP host (optional)')
+    parser.add_argument('--smtp-port', required=False, help='SMTP port (optional)')
+    parser.add_argument('--from-email', required=False, help='From email (for sending emails).')
+    parser.add_argument('--to-email', required=False, help='To email (for sending emails).')
     parser.add_argument('--cache-timeout', required=False, default=21600,
                         help='Timeout for URL caching, in seconds. Defaults to 6 hours.')
     parser.add_argument('--dest-dir', required=False, default='orders/',
@@ -137,10 +230,18 @@ def parse_args():
 
 
 def main():
-    args = parse_args()
-    for year in args.year:
-        AmzScraper(args.user, args.password, year=year, cache_timeout=args.cache_timeout,
-                   orders_dir=args.dest_dir).run()
+    args = vars(parse_args())
+    smtp_args = dict((k, args.pop(k)) for k in args.keys() if k.startswith('smtp_'))
+    if len(smtp_args) == 4:
+        emailer = Emailer(**smtp_args)
+    elif len(smtp_args) == 0:
+        emailer = None
+    else:
+        raise Exception("Did not get 0 or 4 SMTP arguments.")
+    years = args.pop('year')
+    for year in years:
+        AmzScraper(year=year, emailer=emailer, **args).run()
+
 
 if __name__ == "__main__":
     main()
