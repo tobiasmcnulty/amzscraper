a2c893f22ef502869546012ff307bc390d94e706
Tobias McNulty <tobias@caktusgroup.com>
2021-02-22 22:34:02 -0500

    Various cleanup + dead code removal (#3)
    * Probably never going to use mechanize again
    * Memcache was really just for testing, and isn't optional anymore without python-memcache
    * Adds a setup.py
    * Supports setting arguments via env vars

--- amzscraper.py
+++ amzscraper.py
@@ -14,7 +14,6 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 
-import memcache
 from bs4 import BeautifulSoup
 from selenium.common.exceptions import NoSuchElementException
 
@@ -30,57 +29,6 @@ def rand_sleep(max_seconds=5):
     print("done.")
 
 
-class AmzMechanize(object):
-    """ Use Python mechanize to login and retrieve URLs. Broken as of May 2016. """
-
-    headers = [
-        # ('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.13) '
-        #                'Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13')
-        (
-            "User-agent",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 "
-            "(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
-        )
-    ]
-
-    def __init__(self):
-        import mechanize
-
-        self.br = mechanize.Browser()
-        self.br.set_handle_robots(False)
-        self.br.addheaders = self.headers
-
-    def login(self, email, password):
-        self.br.open("https://www.amazon.com")
-        resp = self.br.follow_link(text_regex="Hello. Sign in")
-        self.br.select_form(nr=0)
-        self.br["email"] = email
-        self.br["password"] = password
-        resp = self.br.submit()
-        if resp.code != 200:
-            raise Exception("Got invalid response code %s" % resp.code)
-        elif resp.geturl().startswith(
-            "https://www.amazon.com/ap/signin"
-        ):  # not the URL we wanted
-            html = resp.get_data()
-            soup = BeautifulSoup(html, "lxml")
-            err = soup.find_all("div", attrs={"id": "message_error"})
-            warn = soup.find_all("div", attrs={"class": "a-alert-content"})
-            msg = (
-                err
-                and err[0].renderContents()
-                or (warn and warn[0].renderContents())
-                or html
-            ).strip()
-            raise Exception("Login failed for %s, %s:\n%s" % (email, password, msg))
-
-    def get_url(self, url):
-        return self.br.open(url).get_data()
-
-    def clean_up(self):
-        pass
-
-
 class AmzChromeDriver(object):
     """
     Replacement driver to login to Amazon and download URLs using the Selenium
@@ -90,7 +38,7 @@ class AmzChromeDriver(object):
     def __init__(self):
         from selenium import webdriver
 
-        self.driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+        self.driver = webdriver.Chrome("chromedriver")
         self.driver.implicitly_wait(5)
 
     def login(self, email, password):
@@ -156,7 +104,8 @@ class Emailer(object):
 
         smtp = smtplib.SMTP(self.smtp_host, self.smtp_port)
         smtp.starttls()
-        smtp.login(self.smtp_user, self.smtp_password)
+        if self.smtp_user:
+            smtp.login(self.smtp_user, self.smtp_password)
         smtp.sendmail(send_from, send_to, msg.as_string())
         smtp.close()
 
@@ -182,7 +131,6 @@ class AmzScraper(object):
         user,
         password,
         dest_dir,
-        cache_timeout,
         from_email,
         to_email,
         brcls=AmzChromeDriver,
@@ -190,33 +138,24 @@ class AmzScraper(object):
     ):
         self.year = year
         self.orders_dir = dest_dir
-        self.cache_timeout = cache_timeout
         self.from_email = from_email
         self.to_email = to_email
         self.emailer = emailer
-        self.mc = memcache.Client(["127.0.0.1:11211"], debug=0)
         self.br = brcls()
         self.br.login(user, password)
 
-    def _fetch_url(self, url, use_cache=True):
+    def _fetch_url(self, url):
         key = hashlib.md5(url.encode("utf-8")).hexdigest()
-        val = use_cache and self.mc.get(key.encode("utf-8")) or None
-        if not val:
-            print("fetching %s from server (with random sleep)" % url)
-            val = self.br.get_url(url)
-            rand_sleep()
-            self.mc.set(key, val, self.cache_timeout)
-            from_cache = False
-        else:
-            print("using cache for %s" % url)
-            from_cache = True
-        return val, from_cache
+        print("fetching %s from server (with random sleep)" % url)
+        val = self.br.get_url(url)
+        rand_sleep()
+        return val
 
     def get_order_nums(self):
         order_nums = set()
         url = self.start_url.format(yr=self.year)
         for page_num in itertools.count(start=2, step=1):
-            html, _ = self._fetch_url(url)
+            html = self._fetch_url(url)
             soup = BeautifulSoup(html, "lxml")
             order_links = soup.find_all("a", href=self.order_id_re)
             order_nums |= set(
@@ -238,10 +177,7 @@ class AmzScraper(object):
                 print("skipping order %s (already exists)" % oid)
                 continue
             url = self.order_url.format(oid=oid)
-            html, from_cache = self._fetch_url(url)
-            # force a re-fetch if we got a non-final order from the cache:
-            if "Final Details for Order #" not in html and from_cache:
-                html, _ = self._fetch_url(url, use_cache=False)
+            html = self._fetch_url(url)
             if "Final Details for Order #" not in html:
                 print("skipping order %s (not final)" % oid)
                 continue
@@ -283,26 +219,52 @@ def parse_args():
         description="Scrape an Amazon account and create order PDFs."
     )
     parser.add_argument(
-        "-u", "--user", required=True, help="Amazon.com username (email)."
-    )
-    parser.add_argument("-p", "--password", required=True, help="Amazon.com password.")
-    parser.add_argument("--smtp-user", required=False, help="SMTP username (optional).")
-    parser.add_argument(
-        "--smtp-password", required=False, help="SMTP password (optional)"
-    )
-    parser.add_argument("--smtp-host", required=False, help="SMTP host (optional)")
-    parser.add_argument("--smtp-port", required=False, help="SMTP port (optional)")
-    parser.add_argument(
-        "--from-email", required=False, help="From email (for sending emails)."
+        "-u",
+        "--user",
+        help="Amazon.com username (email).",
+        default=os.environ["AMAZON_USER"],
     )
     parser.add_argument(
-        "--to-email", required=False, help="To email (for sending emails)."
+        "-p",
+        "--password",
+        help="Amazon.com password.",
+        default=os.environ["AMAZON_PASSWORD"],
     )
     parser.add_argument(
-        "--cache-timeout",
+        "--smtp-user",
         required=False,
-        default=21600,
-        help="Timeout for URL caching, in seconds. Defaults to 6 hours.",
+        help="SMTP username (optional).",
+        default=os.environ.get("SMTP_USER"),
+    )
+    parser.add_argument(
+        "--smtp-password",
+        required=False,
+        help="SMTP password (optional)",
+        default=os.environ.get("SMTP_PASSWORD"),
+    )
+    parser.add_argument(
+        "--smtp-host",
+        required=False,
+        help="SMTP host (optional)",
+        default=os.environ.get("SMTP_HOST"),
+    )
+    parser.add_argument(
+        "--smtp-port",
+        required=False,
+        help="SMTP port (optional)",
+        default=os.environ.get("SMTP_PORT"),
+    )
+    parser.add_argument(
+        "--from-email",
+        required=False,
+        help="From email (for sending emails).",
+        default=os.environ.get("FROM_EMAIL"),
+    )
+    parser.add_argument(
+        "--to-email",
+        required=False,
+        help="To email (for sending emails).",
+        default=os.environ.get("TO_EMAIL"),
     )
     parser.add_argument(
         "--dest-dir",
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="amzscraper",
+    version="1.0.0",
+    description="A helpful utility to scrape and email Amazon receipts.",
+    url="https://github.com/tobiasmcnulty/amzscraper",
+    author="Tobias McNulty",
+    author_email="tobias@caktusgroup.com",
+    license="MIT",
+    py_modules=["amzscraper"],
+    install_requires=[
+        "beautifulsoup4==4.9.3",
+        "selenium==3.141.0",
+        "lxml==4.6.2",
+    ],
+    entry_points={
+        "console_scripts": [
+            "amzscraper=amzscraper:main",
+        ]
+    },
+)
